285bc802ed30fff22954091ce8af4a10eff21fbf
pycuber <134294337+pete4radio@users.noreply.github.com>
2025-06-06 18:27:22 -0700

    Added the SAMWISE.h for copying to the sdk boards directory

---
+++ include/boards-SAMWISE.h
@@ -0,0 +1,5 @@
+// Copy this to ~/.pico-sdk/sdk/2.1.1/src/boards/include/boards/SAMWISE.h
+// Then in RPi Extension, set the board to SAMWISE in the lower right or in the top level CMakeLists.txt.
+#include "boards/pico2.h"
+#define PICO_RP2350A 0
+#define PICO_RP2350B 1
